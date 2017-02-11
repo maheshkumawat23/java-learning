385d7bf99cdd240705cc6602d8ce22a310f83f95
Mahesh  Kumawat <1190861@India.TCS.com>
2017-02-11 11:07:33 +0530

    folder updated

--- UseRational.java
+++
@@ -1,9 +0,0 @@
-public class UseRational{
-	
-	public static void main(String args[]){
-		
-		Rational r1= new Rational();
-		r1.set(10,20);
-		r1.show();
-	}
-}
---
+++ simple_porgrams/UseRational.java
@@ -0,0 +1,9 @@
+public class UseRational{
+	
+	public static void main(String args[]){
+		
+		Rational r1= new Rational();
+		r1.set(10,20);
+		r1.show();
+	}
+}
